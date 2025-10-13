debef825f5c08a35461571f56199d962893ce536
Hamdan <Hamdan@DESKTOP-S7R831B>
2025-10-13 19:11:25 +0530

    Add project files.

---
+++ ObjectOrientedProgram/Cars.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedProgram
+{
+    public class Cars : Vehicle
+    {
+        public void Dog()
+        {
+            Console.Write(" Barks");
+        }
+    }
+}
---
+++ ObjectOrientedProgram/Program.cs
@@ -0,0 +1,55 @@
+﻿using ObjectOrientedProgram;
+
+class Inhertitance
+{
+    public static void Main(string[] args)
+    {
+        Cars c = new Cars();
+        Inhertitance i = new Inhertitance();
+        c.Animal();
+        c.Dog();
+
+      
+
+
+    }
+}
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
