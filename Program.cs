e825e7ac162af6d96b29dd64ac33d2cd12d8580f
Ismail-Hamdan12 <146202894+Ismail-Hamdan12@users.noreply.github.com>
2026-03-28 22:50:41 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Abstraction_Interface.Abstraction;
+using Abstraction_Interface.IInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction_Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           /* SubAbst2 s = new SubAbst2();
+            s.Show();
+            SubAbst k = new SubAbst();
+            k.Show();*/
+            Demo d = new Demo();
+            d.Show();
+            MyClass m = new MyClass();
+            m.Show();
+        }
+    }
+}
